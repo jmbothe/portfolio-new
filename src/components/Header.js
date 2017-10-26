d8e94f697168a31a97b42fcc043293516364a498
Jeff Bothe <jeffrymbothe@gmail.com>
2017-10-26 11:51:33 -0500

    deleted commented code from Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,40 +8,6 @@ class Header extends Component {
     this.renderButton = this.renderButton.bind(this)
   }
 
-  // renderButton(section) {
-  //   if (section === this.props.activeSection) {
-  //     return (
-  //       <CSSTransitionGroup
-  //         component="div"
-  //         className="active-button-container"
-  //         transitionName="active-button"
-  //         transitionEnterTimeout={5000}
-  //         transitionLeaveTimeout={5000}
-  //         key={section}
-  //       >
-  //         <button
-  //           key={`${performance.now()}`}
-  //           className={`${section}-button active`} 
-  //           onClick={(e) => this.props.toggleActiveSection(section)}
-  //         >
-  //           {section}
-  //         </button>
-  //       </CSSTransitionGroup>
-  //     )
-  //   } else {
-  //     return (
-  //       <div key={`${performance.now()}`}>
-  //         <button
-  //           className={`${section}-button`} 
-  //           onClick={(e) => this.props.toggleActiveSection(section)}
-  //         >
-  //           {section}
-  //         </button>
-  //       </div>
-  //     )
-  //   }
-  // }
-
   renderButton(section) {
     const name = `${section}-button ${section === this.props.activeSection ? 'active' : ''}`;
     return (
@@ -74,19 +40,6 @@ class Header extends Component {
       </header>
     );
   }
-
-  // render() {
-  //   const sections = ['About', 'Projects', 'Skills', 'Contact', 'Just Because']
-  //   return (
-  //     <header>
-  //       <h1>Jeff Bothe</h1>
-  //       <h2>web developer</h2>
-  //       <nav className="header-buttons">
-  //         {sections.map(item => this.renderButton(item))}
-  //       </nav>
-  //     </header>
-  //   );
-  // }
 }
 
 export default Header;
